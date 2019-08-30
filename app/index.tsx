209aa431373c69c8f2f9d633e659779948fe59b2
joaquin-diaz <joaquindiaz.jd.93@gmail.com>
2019-08-30 14:07:14 -0300

    Add hot load support

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,14 @@
+import 'react-hot-loader';
+
 import * as React from 'react';
+import { hot } from 'react-hot-loader/root';
+
 import reactDom from 'react-dom';
 
 const App = () => {
   return <div>React App</div>;
 };
 
-reactDom.render(<App />, document.getElementById('root'));
+const AppWithHotLoader = hot(App);
+
+reactDom.render(<AppWithHotLoader />, document.getElementById('root'));
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,25 +5,27 @@ module.exports = {
   entry: './app/index.tsx',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
+      template: 'app/index.html',
+    }),
+  ],
 };
