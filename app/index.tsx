bbf0eaea626792341558574ef31dbc365fc9e7fc
Joaquin Diaz <joaquin_fdv@goshippo.com>
2019-08-30 13:31:24 -0300

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import reactDom from 'react-dom';
+
+const App = () => {
+  return <div>React App</div>;
+};
+
+reactDom.render(<App />, document.getElementById('root'));
